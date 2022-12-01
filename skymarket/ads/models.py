22414faca5aca8fe3164cdba19d35c65ca16df3a
SergeyLubash <luba-sergei@yandex.ru>
2022-12-01 09:14:29 +0300

    fixed the bugs

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Ad(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media/', null=True)
     title = models.CharField(max_length=100, verbose_name='Название товара')
     price = models.PositiveIntegerField()
     description = models.TextField()
